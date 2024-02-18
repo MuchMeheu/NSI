d3fcbe14abdb1c5b337072cdcff6001e0b1aa7cd
Meheu <40333002+MuchMeheu@users.noreply.github.com>
2024-02-18 15:46:08 +0400

    14

---
+++ Theme D/tpjavascript/exercice1/exercice1.js
@@ -0,0 +1,6 @@
+var width = prompt("Entrez la largeur du rectangle :");
+var length = prompt("Entrez la longueur du rectangle :");
+
+var area = width * length;
+
+alert("L'aire du rectangle est : " + area);
---
+++ Theme D/tpjavascript/interaction/interraction.js
@@ -0,0 +1,6 @@
+function change_couleur() {
+    var paragraphe = document.getElementById("important");
+    paragraphe.style.color="red"
+    console.log(paragraphe);
+
+    }
