5cff6e086a7e4a60929c67be8129ca4ac93a3d96
Meheu <40333002+MuchMeheu@users.noreply.github.com>
2024-02-28 11:38:03 +0400

    28 feb 2024 #2

--- Theme G/MiniProjet.py
+++ Theme G/MiniProjet.py
@@ -1,3 +1,5 @@
+#Sujet 1: 441 eratosthene
+
 def eratosthene(n):
     crible = [True] * (n + 1)
     crible[0] = False
@@ -23,3 +25,38 @@ assert eratosthene(6) == [False, False, True, True, False, True, False]
 assert premiers_jusque(5) == [2, 3, 5]
 assert premiers_jusque(6) == [2, 3, 5]
 assert premiers_jusque(20) == [2, 3, 5, 7, 11, 13, 17, 19]
+
+print(premiers_jusque(20))
+
+#Sujet 2: 440 tri pascal
+
+def pascal(n):
+    triangle = [[1]]
+    for k in range(1, n+1):
+        ligne_k = [1]
+        for i in range(1, k):
+            ligne_k.append(triangle[k-1][i - 1] + triangle[k-1][i])
+        ligne_k.append(1)
+        triangle.append(ligne_k)
+    return triangle
+
+
+# tests
+assert pascal(4) == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+]
+
+assert pascal(5) == [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+    [1, 5, 10, 10, 5, 1],
+]
+
+print(pascal(4))
